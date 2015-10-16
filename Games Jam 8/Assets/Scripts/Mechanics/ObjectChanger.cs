847cd812df2682bc437aa50880cc8be0a36bb132
James Horsley <decardcain37@gmail.com>
2015-10-17 01:44:48 +1100

    [Stuff]

--- Games Jam 8/Assets/Scripts/Mechanics/ObjectChanger.cs
+++ Games Jam 8/Assets/Scripts/Mechanics/ObjectChanger.cs
@@ -1,21 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ObjectChanger : MonoBehaviour
 {
-	public GameObject[] objects;
-	public string objectName;
+
+	public GameObject[] objectInspector;
+	public GameObject[] objectInScene;
+	public List<GameObject> objectPrefabs, objectSelection;
+	public string tag;//can be changed to objectName
+
+
 
 	private void Start()
 	{
-		gameObject.tag = "CollectibleItem";
-		objectName = gameObject.name;
-		ChangeObject();
+		getModelInScene();
+		getModelInspector();
 	}
 
-	private void ChangeObject()
+	private void getModelInScene()
 	{
-		int randomI = Random.Range(0, objects.Length);
-		gameObject.GetComponent<MeshFilter>().mesh = objects[randomI].GetComponent<MeshFilter>().sharedMesh;
+		objectSelection.AddRange(GameObject.FindGameObjectsWithTag(name));
+	}
+
+	
+	private void getModelInspector()
+	{
+		for(int i=0; i < objectInspector.Length; i++)
+		{
+			objectPrefabs.Add (objectInspector[i].gameObject as GameObject);
+		}
+	}
+
+	private void selectRandomGameObject()
+	{
+		getRenderer(objectPrefabs[randomPoint(objectPrefabs.Count)]).material = getRenderer(objectSelection[randomPoint(objectSelection.Count)]).material;
+	}
+
+	private Renderer getRenderer(GameObject savedObject)
+	{
+		Renderer savedRender = savedObject.GetComponent<Renderer>();
+		return savedRender;
+	}
+
+	private int randomPoint(int lengthOfArray)
+	{
+		int randomPoints = Random.Range (0, lengthOfArray);
+		return randomPoints;
 	}
 }
+
+/*
+	Set objectPrefabs to objectInspector
+	Set ObjectSelection to objectInspector
+	Find all objects with collectibleObject tag and store them in the objectInScene array
+	Select the value from the top of the objectPrefabs list and select a random value from objectSelection
+	
+ */
+
+
+
+
--- Games Jam 8/Assets/Scripts/Player/Utilities/createGoal.cs
+++ Games Jam 8/Assets/Scripts/Player/Utilities/createGoal.cs
@@ -5,25 +5,28 @@ using System.Collections.Generic;
 public class createGoal : MonoBehaviour
 {
 	public List<GameObject> collectibleObjects;
+	//public GameObject[] collectibleObjects;
 	public List<string> collectibleNames;
 	public List<GameObject> item;
-	public List<item> items;
+	//public List<item> items;
 
 	private void Start()
 	{
 		findAllCollectibleObject();
+		sortAllCollectibleObjects();
 	}
 
 	private void findAllCollectibleObject()
 	{
 		collectibleObjects.AddRange(GameObject.FindGameObjectsWithTag("CollectibleItem"));
+		//collectibleObjects.Add = GameObject.FindGameObjectsWithTag("CollectibleItem");
 	}
 
 	private void sortAllCollectibleObjects()
 	{
 		for(int i = 0; i < collectibleObjects.Count; i++)
 		{
-
+			
 		}
 	}
 }
