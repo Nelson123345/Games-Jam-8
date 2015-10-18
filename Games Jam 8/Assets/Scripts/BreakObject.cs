356beb0508008031f1e07773975a09d51ab53c5a
Nelson Burge <nelson.burge@live.co.uk>
2015-10-18 13:02:05 +1100

    [MORE FUCKING COLLIDERS AND OTHER FUCKING SHIT]

---
+++ Games Jam 8/Assets/Scripts/BreakObject.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakObject : MonoBehaviour
+{
+	public Rigidbody gameObjectRigidBody;
+	private Vector3 lastVelocity;
+	public GameObject gameObjectToBreak;
+	public GameObject brokenVersion;
+	public Material brokenTexture; //Leave blank if there isn't one
+	public Vector3 rotationOffset;
+	public Vector3 positionOffset;
+	private bool fractured;
+
+	public float fractureAmplification;
+
+	public float maxForce;
+
+	private void Update()
+	{
+		lastVelocity = gameObjectRigidBody.velocity;
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			fractureObject();
+		}
+	}
+
+	public void fractureObject()
+	{
+		if(!fractured)
+		{
+			fractured = true;
+			Quaternion objRot = Quaternion.Euler(gameObjectToBreak.transform.eulerAngles + rotationOffset);
+			GameObject fractureParent = GameObject.Instantiate(brokenVersion, gameObjectToBreak.transform.position + positionOffset, objRot) as GameObject;
+			Transform[] fragments = fractureParent.GetComponentsInChildren<Transform>();
+				
+			if(brokenTexture != null)
+			{
+				setMaterialForTransformArray(fragments, brokenTexture);
+			}
+			else
+			{
+				setMaterialForTransformArray(fragments, gameObjectToBreak.GetComponent<Renderer>().material);
+			}
+			Destroy(gameObjectToBreak);
+		}
+	}
+
+	public void setMaterialForTransformArray(Transform[] array, Material mat)
+	{
+		for(int i = 1; i < array.Length; i++)
+		{
+			if(array[i].gameObject.GetComponent<fragmentMaterialException>() == false)
+			{
+				Renderer fragmentRenderer = array[i].gameObject.GetComponent<Renderer>();
+				fragmentRenderer.material = mat;
+/*				array[i].GetComponent<Rigidbody>().AddForce(lastVelocity * fractureAmplification, ForceMode.Impulse);
+*/			}
+		}
+	}
+
+	private void checkForFracture(Vector3 velocity, float maxForce)
+	{
+		float totalforce = Mathf.Abs(velocity.x) + Mathf.Abs(velocity.y) + Mathf.Abs(velocity.z);
+
+		//print(totalforce);
+
+		if(totalforce > maxForce)
+		{
+			fractureObject();
+		}
+	}
+
+	private void OnCollisionEnter(Collision collidedGameObject)
+	{
+		Rigidbody collidedGameObjectRigidbody = collidedGameObject.gameObject.GetComponent<Rigidbody>();
+		if(collidedGameObjectRigidbody)
+		{
+			Vector3 velocityProduct;
+			//print("Last Velocity: " + lastVelocity);
+
+			velocityProduct = (collidedGameObjectRigidbody.velocity += gameObjectRigidBody.velocity);
+			//print(velocityProduct);
+
+			checkForFracture(velocityProduct, maxForce);
+		}
+		else
+		{
+			//print("Last Velocity: " + lastVelocity);
+			checkForFracture(lastVelocity, maxForce);
+		}
+	}
+}
---
+++ Games Jam 8/Assets/SwapObjectInEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SwapObjectInEditor : MonoBehaviour
+{
+	public GameObject objToReplace;
+	public bool toggle;
+	private void Update()
+	{
+		if(toggle)
+		{
+			replaceObject();
+			toggle =! toggle;
+		}
+	}
+
+	private void replaceObject()
+	{
+		GameObject newGameobject = GameObject.Instantiate(objToReplace) as GameObject;
+		newGameobject.transform.parent = gameObject.transform.parent;
+		newGameobject.transform.rotation = gameObject.transform.rotation;
+		newGameobject.transform.localScale = gameObject.transform.localScale;
+		newGameobject.transform.position = gameObject.transform.position;
+		DestroyImmediate(gameObject);
+	}
+}
