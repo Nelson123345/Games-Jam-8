c3274b29eff20f8d4959987a065a234f9f5ec821
James Horsley <decardcain37@gmail.com>
2015-10-18 16:48:32 +1100

    [UI WORK]

---
+++ Games Jam 8/Assets/buttonPresses.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonPresses : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject myUI;
+
+	public void BACK()
+	{
+		print ("aa");
+		target.SetActive(true);
+		myUI.SetActive(false);
+	}
+}
---
+++ Games Jam 8/Assets/startLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startLevel : MonoBehaviour {
+
+	public void LOADLEVEL()
+	{
+		Application.LoadLevel(1);
+	}
+	public void QUIT()
+	{
+		Application.Quit();
+	}
+}
